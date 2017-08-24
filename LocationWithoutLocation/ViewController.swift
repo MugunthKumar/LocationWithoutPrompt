14d94fccf75adde9325adc759e50e54fafd62ae6
Mugunth <mugunth.kumar@gmail.com>
2017-08-24 11:55:21 +0800

    Initial Commit

---
+++ LocationWithoutLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationWithoutLocation
+//
+//  Created by Mugunth Kumar on 24/8/17.
+//  Copyright © 2017 Steinlogic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
